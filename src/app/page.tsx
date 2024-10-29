0ae1b8e299a40ea854b8deda94d08ccd2b64ba97
mickael keita <keitamickael@gmail.com>
2024-10-29 12:38:00 +0100

    ajout d'une page d'accueil

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,54 @@
-import Image from "next/image";
+import { FaInstagram, FaFacebook, FaHeart } from "react-icons/fa";
+import { BsTwitterX } from "react-icons/bs";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      <header className="w-full max-w-[1600px] mx-auto flex items-center justify-between mb-[100px]">
+        <div className="flex items-center gap-4 text-4xl text-white font-bold py-5">
+          <FaHeart />
+          <h1 className="text-4xl text-white font-bold">Flamme</h1>
+        </div>
+        <nav className="flex items-center gap-10">
+          <ul className="flex items-center gap-4 text-4xl text-white">
+            <li className="cursor-pointer">
+              <FaInstagram />
+            </li>
+            <li className="cursor-pointer">
+              <BsTwitterX />
+            </li>
+            <li className="cursor-pointer">
+              <FaFacebook />
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main className="w-full max-w-[1600px] mx-auto text-white">
+        <div className="max-w-xl">
+          <h1 className="text-5xl font-bold text-black-900 mb-10">Flamme</h1>
+          <p className="mb-10 text-xl">
+            Bienvenue sur Flamme, la plateforme de rencontre moderne qui réunit
+            les célibataires en quête de leur moitié. Avec Flamme, chaque swipe
+            est une chance de découvrir une personne qui partage vos passions et
+            vos envies. Que vous soyez en quête d&apos;une relation sérieuse ou
+            que vous souhaitiez simplement faire de nouvelles rencontres, notre
+            approche simplifiée et authentique rend chaque interaction naturelle
+            et engageante.
+          </p>
+          <ul className="flex items-center gap-4">
+            <li>
+              <button className="bg-white text-rose-400 p-2 text-xl font-bold rounded-3xl">
+                Inscription
+              </button>
+            </li>
+            <li>
+              <button className="bg-rose-400 text-wite p-2 text-xl font-bold rounded-3xl">
+                Connexion
+              </button>
+            </li>
+          </ul>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    </>
   );
 }
